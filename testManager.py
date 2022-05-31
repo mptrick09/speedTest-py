9c8b3e1b4d4cba18f32cbfbdf81ec6f378ffc011
Jean Lambert <jniyigaba@gmail.com>
2022-05-31 16:05:01 +0200

    testManager modified

--- testManager.py
+++ testManager.py
@@ -1,24 +1,21 @@
 import speedtest
 class TestManager:
     def __init__(self, mockData):
+        self.promptConfig()
         self.results = []
         self.mockData = mockData
         self.__servers = []
         self.__speedTest = None
-        self.__interval = 0
-        self.__execution = 0
+        
         
     def speedTest(self):
         
         self.__speedTest = speedtest.Speedtest()
-        self.__servers.append(self.__speedTest.get_best_server())
+        self.__speedTest.get_servers(self.__servers)
         self.__speedTest.get_best_server()
-        threads = None
-        self.__speedTest.download(threads=threads)
-        self.__speedTest.upload(threads=threads)
-        self.__speedTest.results.share()
         
-        self.results.append(self.__speedTest.results.dict())
+        self.results.append(self.__speedTest.results.download)
+        self.results.append(self.__speedTest.results.upload)
         
     def promptConfig(self):
         self.__interval = int(input("Gibe ein interval ein: "))
